16d951223771fb3b8c450b938691e48070e78fd2
Raghul263 <raghul.2201263@srec.ac.in>
2026-03-31 13:06:11 +0530

    Final absolute symmetry, camera lock, and stability fixes

--- app/layout.js
+++ app/layout.js
@@ -8,7 +8,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>
+      <body suppressHydrationWarning>
         <div id="root">{children}</div>
       </body>
     </html>
